f9ce661dc98d62d7aba0e5a1ebb9b0f292e80770
achyutha11 <57967977+achyutha11@users.noreply.github.com>
2020-07-06 16:00:59 +0530

    Update gen_ld_mat.py

--- src/gen_ld_mat.py
+++ src/gen_ld_mat.py
@@ -28,8 +28,25 @@ def est_kxp_mat(gt_mat, pop_vec, pop, K=500):
     return(kxp_ld_mat)
 
 
+
 # TODO : can we write methods here to take in the output of the above function
 # and then generate LD-summaries?
 
+
+#def mat_split(gt_mat, pop_vec, pop,X):
+#     """
+#        Arguments:
+#            gt_mat: a 0/1/2 genotype matrix with P rows and N columns
+#            pop_vec:  a N-length vector of population labels
+#            pop: a population label for individuals in the broader dataset
+#            (e.g. 'CEU')
+#            X: number of segments to split the genotype matrix into
+#    """
+#    P,N = gt_mat.shape
+#    assert(pop_vec.size==N)
+#    gt_pop_filt=gt_mat[:,(pop_vec==pop)]
+#    for i in tqdm(range(X)):
+#        return gt_pop_filt((((P/X)*i):((P/X)*(i+1))),:)
+     
 
 
