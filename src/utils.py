cdbe2c84cbbab63e292c41edf840411756f232d9
achyutha11 <57967977+achyutha11@users.noreply.github.com>
2020-08-04 14:36:17 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -44,19 +44,20 @@ def kxp_file_convert(kxp_file):
     
     return kxp_convert(kxpmat)
 
-def ragged_convert(array_list,blen): 
+def ragged_convert(array_list,blen):
     """
     Convert list of adaptive-K arrays to P x P matrix where P is the number of SNPs
     Arguments:
-    array_list : List of arrays of different lengths
-    blen : block length used to generate the list of arrays 
+    array_list : List of adaptive-K arrays
+    blen : Block length used in the function to generate adaptive arrays
     """
+    
     assert(blen < len(array_list))
     assert(len(array_list) > 1)
-    P = len(array_list)
+    P = len(array_list) 
     
     # Creating empty matrix
-    mat = np.zeros((P,P))
+    mat = np.zeros((P+1,P+1))
     
     # Iterating list of arrays and appending each list to appropriate position of P x P matrix
     for i in tqdm(range(P)):
@@ -66,22 +67,42 @@ def ragged_convert(array_list,blen):
             elif array_list[i].shape[0] == 0:
                 continue
             elif array_list[i].shape[0] > 0 and array_list[i].shape[0] < blen:
-                sub_indices = []
-                for j in range(array_list[i].shape[0]):
-                    sub_indices.append(j)
-                if all(array_list[i][m].shape[0]%blen==0 for m in sub_indices):
+                if all(array_list[i][m].shape[0]%blen==0 for m in list(range(array_list[i].shape[0]))):
                     index = array_list[i].shape[0]*blen
                     mat[i+1:index+i+1,i] = np.concatenate(array_list[i]) 
                 else:
-                    sums = []
+                    lengths = []
                     for k in range(array_list[i].shape[0]):
-                        sums.append(array_list[i][k].shape[0])
-                    index = sum(sums)
+                        lengths.append(array_list[i][k].shape[0])
+                    index = sum(lengths)
                     mat[i+1:index+i+1,i] = np.concatenate(array_list[i]) 
         else:
             mat[i+1:array_list[i].shape[0]+i+1,i] = array_list[i]       
     return mat
 
+def adaptive_file_convert(adaptive_file,blen):
+    """
+    Convert adaptive-K file to P x P matrix where P is the number of SNPs
+    Arguments:
+    adaptive_file : Npz file containing adaptive array and indices of array endpoints
+    blen : Block length used in the function to generate adaptive arrays
+    """
+    
+    # Load in concatenated adaptive array and indices of array endpoints
+    inter = np.load(adaptive_file)
+    adaptive_mat = inter['adaptive_ld_mat']
+    idxs = inter['idx']
+    
+    
+    array_list = []
+    
+    # Create list of arrays using endpoints from idxs
+    for i in tqdm(range(idxs.size-1)):
+    array_list.append(adaptive_ld_mat[idxs[i]:idxs[i+1]])
+    
+    # Convert list of arrays to P x P matrix
+    return ragged_convert(array_list,blen)
+ 
 
 def stack_ragged(array_list, axis=0):
     """
