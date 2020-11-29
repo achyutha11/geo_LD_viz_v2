b471eccd26cff0875fee61e3778e325f82eafb7a
achyutha11 <57967977+achyutha11@users.noreply.github.com>
2020-11-29 16:08:23 +0530

    Update plotting.py

--- src/plotting.py
+++ src/plotting.py
@@ -9,7 +9,7 @@ import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import cartopy.feature as cf
 
-def cartopy_plot(values, coordinates):
+def cartopy_plot(values, coordinates, map_color=False, title, marker='o', marker_color='red'):
     """
     Plot LD statistics on a global map (projection: equirectangular)
     
@@ -20,7 +20,16 @@ def cartopy_plot(values, coordinates):
     Output:
         plot: Plot with specified values at required points on global map
     """
+    assert len(values) == len(coordinates)
     
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.coastlines()
+    if map_color:
+        ax.stock_img()
+    plt.title(title)
+    
+    for index, lat_long in enumerate(coordinates):
+        plt.plot(lat_long[0], lat_long[1], markersize=(10*values[index]),marker=marker,color=marker_color)
     
     
     
